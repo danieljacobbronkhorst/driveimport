f52257ca33d737f3c485939f3a56f9eb9e83d02f
danieljacobbronkhorst <daniel.jacob.bronkhorst@gmail.com>
2025-09-03 10:29:40 +0200

    Add files via upload

--- importattendance.py
+++ importattendance.py
@@ -38,6 +38,8 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920,1080")
 
+chrome_options.binary_location = "/usr/bin/chromium-browser"
+
 driver = webdriver.Chrome(options=chrome_options)
 
 # ----------------------------
