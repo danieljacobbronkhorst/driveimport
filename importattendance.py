db5d19a52867790a8cc290503ed58f5a9984d846
danieljacobbronkhorst <daniel.jacob.bronkhorst@gmail.com>
2025-09-03 15:27:02 +0200

    Update importattendance.py

--- importattendance.py
+++ importattendance.py
@@ -49,6 +49,7 @@ chrome_options.add_argument("--disable-popup-blocking")
 chrome_options.add_argument("--disable-notifications")
 chrome_options.add_argument("--disable-default-apps")
 chrome_options.add_argument("--disable-web-security")
+chrome_options.add_argument("--ignore-certificate-errors")
 chrome_options.add_argument("--disable-infobars")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-browser-side-navigation")
@@ -338,6 +339,7 @@ if failed_entries:
 # CLOSE DRIVER
 # ----------------------------
 driver.quit()
+
 
 
 
